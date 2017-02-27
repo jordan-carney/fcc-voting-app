9567cc13a0742dc94af4e22d71e8b02e9aed4740
Jordan Carney <jordan@jordancarney.com>
2017-02-26 21:39:35 -0500

    Patched has-voted status detection on User poll listings pages

--- routes/user-poll-list/router.js
+++ routes/user-poll-list/router.js
@@ -3,12 +3,44 @@ const models = require('../models')
 const Poll = models.Poll
 
 router.get('/:user', function *(next) {
+  // IP fix for Heroku 
+  let ipAddress = this.request.ip
+  const xip = this.headers["x-forwarded-for"];
+  if (xip){
+    ipAddress = xip
+  }
   const polls = yield Poll.find({ createdBy: new RegExp('^' + this.params.user + '$', 'i') })
   if (polls.length) {
+    polls.forEach( poll => {
+      poll.hasVoted = poll.voters.some( ip => ipAddress === ip)
+    })
     this.render('poll-list/user', {
       csrfToken: this.csrf,
-      polls: polls
+      openPolls: polls
     })
+  }
+
+  yield next
+})
+
+router.post('/:user', function *(next) {
+  const pollID = this.request.body.pollID
+  const vote = this.request.body.vote
+
+  let ipAddress = this.request.ip
+  const xip = this.headers["x-forwarded-for"];
+    if (xip){
+      ipAddress = xip
+    }
+
+  try {
+    const pollID = this.request.body.pollID
+    const vote = this.request.body.vote
+    yield Poll.update({_id: pollID, 'options.title': vote, 'voters': {$nin: [ipAddress]}}, {$inc: {'options.$.votes': 1}, $addToSet: {'voters': ipAddress}})
+    //Redirect to that poll's dedicated page
+    this.redirect('/' + this.params.user)
+  } catch(err) {
+    console.log(err)
   }
 
   yield next
